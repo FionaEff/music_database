a56c60509f9988366a55ed7c7909f51245cb3e37
FionaEff <f.freidinger@outlook.de>
2026-03-12 14:33:04 +0100

    added comments

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
+# Creating and setting up Flask app
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
@@ -17,13 +18,16 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Defining blueprints for packages
     from app.errors import bp as errors_bp
 
     app.register_blueprint(errors_bp)
+
     from app.main import bp as main_bp
 
     app.register_blueprint(main_bp)
 
+    # App logging
     if not app.debug and not app.testing:
 
         if not os.path.exists("logs"):
--- config.py
+++ config.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
 
-
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "dev-key" # Change dev-key to random key for productive use
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
